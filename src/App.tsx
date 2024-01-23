0f7e34291866f5d21964b0631a1f77b06a05bd6d
Samuel <samuelrivba@gmail.com>
2024-01-22 20:21:14 -0500

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Assets/global.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-green-500">Hello World</h1>;
 }
 
 export default App;
